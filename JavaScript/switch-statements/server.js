79a3a5ff3a1569eacc7e18d9659025d65fee1504
singharaj usai <djjd22938@gmail.com>
2023-04-18 20:37:04 -0400

    switch

---
+++ JavaScript/switch-statements/server.js
@@ -0,0 +1,11 @@
+let dayOfWeek = "Wednesday";
+switch (dayOfWeek) {
+  case "Monday":
+    console.log("It's Monday!");
+    break;
+  case "Tuesday":
+    console.log("It's Tuesday!");
+    break;
+  default:
+    console.log("It's not Monday or Tuesday.");
+}
