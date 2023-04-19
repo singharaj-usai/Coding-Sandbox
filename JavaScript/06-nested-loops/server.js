569abf8253241d27c5f682abfda79ea5f4a30c91
singharaj usai <djjd22938@gmail.com>
2023-04-18 20:43:54 -0400

    nested loops

---
+++ JavaScript/06-nested-loops/server.js
@@ -0,0 +1,10 @@
+let matrix = [  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9]
+];
+
+for (let i = 0; i < matrix.length; i++) {
+  for (let j = 0; j < matrix[i].length; j++) {
+    console.log(matrix[i][j]);
+  }
+}
